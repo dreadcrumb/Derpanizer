1f231b05735f4b006fe86829cc3f32a9a5be0652
Leander Suda <leander.suda@aon.at>
2018-05-07 22:00:02 +0200

    Added Zoom. Added user study files

--- Derpanizer/Assets/Scripts/GameManagerScript/GameManager.cs
+++ Derpanizer/Assets/Scripts/GameManagerScript/GameManager.cs
@@ -4,105 +4,111 @@ using UnityEngine;
 
 namespace Assets.Scripts.GameManagerScript
 {
-	public class GameManager : MonoBehaviour
-	{
-		public static GameManager Manager;
-		private FileManager _mFileManager;
-		private SaveAndLoadScript _mSaveScript;
-		private SaveClass _loadFile;
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Manager;
+        private FileManager _mFileManager;
+        private SaveAndLoadScript _mSaveScript;
+        private SaveClass _loadFile;
 
-		private string _mPath;
+        private string _mPath;
 
-		private void Awake()
-		{
-			if (Manager == null)
-			{
-				DontDestroyOnLoad(gameObject);
-				Manager = this;
-			}
-			else if (Manager != this)
-			{
-				CopyManager(Manager);
-				Destroy(Manager);
-				InitFileManager();
-			}
-		}
+        private void Awake()
+        {
+            if (Manager == null)
+            {
+                DontDestroyOnLoad(gameObject);
+                Manager = this;
+            }
+            else if (Manager != this)
+            {
+                CopyManager(Manager);
+                Destroy(Manager);
+                InitFileManager();
+            }
+        }
 
-		private void CopyManager(GameManager mngr)
-		{
-			_mFileManager = mngr._mFileManager;
-			_mSaveScript = mngr._mSaveScript;
-			_mPath = mngr._mPath;
-			_loadFile = mngr._loadFile;
-		}
+        private void CopyManager(GameManager mngr)
+        {
+            _mFileManager = mngr._mFileManager;
+            _mSaveScript = mngr._mSaveScript;
+            _mPath = mngr._mPath;
+            _loadFile = mngr._loadFile;
+        }
 
-		private void Start()
-		{
-			_mSaveScript = gameObject.AddComponent<SaveAndLoadScript>();
-		}
+        private void Start()
+        {
+            _mSaveScript = gameObject.AddComponent<SaveAndLoadScript>();
+        }
 
-		private void Update()
-		{
-			if (Input.GetKeyDown(KeyCode.O) && gameObject.GetComponent<FileManager>() != null)
-			{
-				gameObject.GetComponent<FileManager>().UpdateFileLocations();
-				gameObject.GetComponent<SaveAndLoadScript>().Save();
-			}
-			else if (Input.GetKeyDown(KeyCode.R))
-			{
-				var sgs = new StartGameScript();
-				SetPath(sgs.InitDebug());
-				sgs.LoadNewScene();
-			}
-			//else if (Input.GetKeyDown(KeyCode.B) && gameObject.GetComponent<FileManager>() != null)
-			//{
-			//	gameObject.GetComponent<FileManager>().InitNewBox();
-			//}
-		}
-		
-		public FileManager GetFileManager()
-		{
-			return gameObject.GetComponent<FileManager>();
-		}
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.O) && gameObject.GetComponent<FileManager>() != null)
+            {
+                gameObject.GetComponent<FileManager>().UpdateFileLocations();
+                gameObject.GetComponent<SaveAndLoadScript>().Save();
+            }
+            //else if (Input.GetKeyDown(KeyCode.R))
+            //{
+            //	var sgs = new StartGameScript();
+            //	SetPath(sgs.InitDebug());
+            //	sgs.LoadNewScene();
+            //}
+            //else if (Input.GetKeyDown(KeyCode.B) && gameObject.GetComponent<FileManager>() != null)
+            //{
+            //	gameObject.GetComponent<FileManager>().InitNewBox();
+            //}
 
-		public void SetPath(string path)
-		{
-			_mPath = path;
-		}
+            float fov = Camera.main.fieldOfView;
+            fov += Input.GetAxis("Mouse ScrollWheel") * -25;
+            fov = Mathf.Clamp(fov, 15f, 90f);
+            Camera.main.fieldOfView = fov;
 
-		public string GetPath()
-		{
-			return _mPath;
-		}
+        }
 
-		public void LoadGame()
-		{
-			var sgs = new StartGameScript();
-			_loadFile = _mSaveScript.Load();
-			sgs.LoadNewScene();
-		}
+        public FileManager GetFileManager()
+        {
+            return gameObject.GetComponent<FileManager>();
+        }
 
-		public void StartGame()
-		{
-			var sgs = new StartGameScript();
-			SetPath(sgs.InitRootPath());
-			sgs.LoadNewScene();
-		}
+        public void SetPath(string path)
+        {
+            _mPath = path;
+        }
 
-		private void InitFileManager()
-		{
-			_mFileManager = gameObject.AddComponent<FileManager>();
+        public string GetPath()
+        {
+            return _mPath;
+        }
 
-			if (_loadFile == null)
-			{
-				_mFileManager.InitFromScratch(_mPath);
-			}
-			else
-			{
-				_mFileManager.InitFromLoadFile(_loadFile);
-			}
+        public void LoadGame()
+        {
+            var sgs = new StartGameScript();
+            _loadFile = _mSaveScript.Load();
+            sgs.LoadNewScene();
+        }
 
-		}
+        public void StartGame()
+        {
+            var sgs = new StartGameScript();
+            SetPath(sgs.InitRootPath());
+            sgs.LoadNewScene();
+        }
 
-	}
+        private void InitFileManager()
+        {
+            _mFileManager = gameObject.AddComponent<FileManager>();
+
+            if (_loadFile == null)
+            {
+                _mFileManager.InitFromScratch(_mPath);
+            }
+            else
+            {
+                _mFileManager.InitFromLoadFile(_loadFile);
+            }
+
+        }
+
+    }
 }
